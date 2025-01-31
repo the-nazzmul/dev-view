b609e4171702964c467617606f2f76a6014ac700
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-01-31 15:00:56 +0600

    added call configuring login in the meeting setup component

--- src/components/MeetingSetup.tsx
+++ src/components/MeetingSetup.tsx
@@ -1,4 +1,37 @@
-const MeetingSetup = () => {
+"use client";
+
+import { useCall } from "@stream-io/video-react-sdk";
+import { useEffect, useState } from "react";
+
+const MeetingSetup = ({ onSetupComplete }: { onSetupComplete: () => void }) => {
+  const [isCameraDisabled, setIsCameraDisabled] = useState<boolean>(true);
+  const [isMicDisabled, setIsMicDisabled] = useState<boolean>(false);
+
+  const call = useCall();
+
+  if (!call) return null;
+
+  useEffect(() => {
+    if (isCameraDisabled) {
+      call.camera.disable();
+    } else {
+      call.camera.enable();
+    }
+  }, [isCameraDisabled, call.camera]);
+
+  useEffect(() => {
+    if (isMicDisabled) {
+      call.microphone.disable();
+    } else {
+      call.microphone.enable();
+    }
+  }, [isMicDisabled, call.microphone]);
+
+  const handleJoin = async () => {
+    await call.join();
+    onSetupComplete();
+  };
+
   return <div>MeetingSetup</div>;
 };
 
