0238d1de715163d151e157b8eb90c05b4a8f96a6
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-02-03 20:44:19 +0600

    built user avatar component

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,5 +1,21 @@
-const UserInfo = ({ user }) => {
-  return <div>UserInfo</div>;
+import { UserCircle2Icon } from "lucide-react";
+import { Doc } from "../../convex/_generated/dataModel";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+type User = Doc<"users">;
+
+const UserInfo = ({ user }: { user: User }) => {
+  return (
+    <div className="flex items-center gap-2">
+      <Avatar className="size-6">
+        <AvatarImage src={user.image} />
+        <AvatarFallback>
+          <UserCircle2Icon className="size-4" />
+        </AvatarFallback>
+      </Avatar>
+      <span>{user.name}</span>
+    </div>
+  );
 };
 
 export default UserInfo;
